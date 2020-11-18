061344e75737d8478c4914da23bf58bfb06fc57d
nmyers217 <nick.b.myers@protonmail.com>
2020-11-17 23:55:00 -0600

    Finish day 7

--- src/day_07.jl
+++ src/day_07.jl
@@ -118,54 +118,94 @@ function advance_machine!(machine::IntCodeMachine)
     end
 end
 
+function setup_amplifiers(program, phases)::Array{IntCodeMachine,1}
+    machines = map(phases) do phase
+        m = IntCodeMachine(copy(program))
+        push!(m.stdin, phase)
+        m
+    end
+    push!(machines[1].stdin, 0)
+    machines
+end
+
+function run_amplifiers!(amplifiers::Array{IntCodeMachine,1})
+    last_outputs = []
+    i = 1
+    while true
+        m = amplifiers[i]
+        while length(last_outputs) > 0
+            push!(m.stdin, popfirst!(last_outputs))
+        end
+        advance_machine!(m)
+        while length(m.stdout) > 0
+            push!(last_outputs, popfirst!(m.stdout))
+        end
+        if i == length(amplifiers) && m.terminated
+            return last_outputs[end]
+        else
+            i += 1
+            if i > length(amplifiers)
+                i = mod1(i, length(amplifiers))
+            end
+        end
+    end
+end
+
+function permutations(min=0, max=5)
+    result = []
+
+    (recur_helper(arr) = begin
+        if length(arr) == (max - min)
+            return push!(result, arr)
+        end
+
+        for n in min:(max - 1)
+            if !(n in arr)
+                recur_helper([arr..., n])
+            end
+        end
+    end)([])
+
+    result
+end
+
 function solve()
-    open("res/day_05.txt") do input_file
+    open("res/day_07.txt") do input_file
         program = [
             parse(Int, x) for x in split(strip(read(input_file, String)), ",")
         ]
 
-        m = IntCodeMachine(program)
-        push!(m.stdin, 1)
-        advance_machine!(m)
+        max = -Inf
+        for phases in permutations()
+            output = run_amplifiers!(setup_amplifiers(program, phases))
+            if output > max
+                max = output
+            end
+        end
+        part_one = max
 
-        @test all(n -> n == 0, m.stdout[1:(end - 1)])
-
-        part_one = m.stdout[end]
-
-        m = IntCodeMachine(program)
-        push!(m.stdin, 5)
-        advance_machine!(m)
-
-        part_two = m.stdout[end]
+        max = -Inf
+        for phases in permutations(5, 10)
+            output = run_amplifiers!(setup_amplifiers(program, phases))
+            if output > max
+                max = output
+            end
+        end
+        part_two = max
 
         part_one, part_two
     end
 end
 
 function run_tests()
-    run(prog, inputs) = begin
-        m = IntCodeMachine(prog)
-        push!(m.stdin, inputs...)
-        advance_machine!(m)
-        m.stdout
-    end
-    @testset "IntCodeMachine" begin
-        @test run([3,9,8,9,10,9,4,9,99,-1,8], [8]) == [1]
-        @test run([3,9,8,9,10,9,4,9,99,-1,8], [9]) == [0]
-        @test run([3,9,7,9,10,9,4,9,99,-1,8], [7]) == [1]
-        @test run([3,9,7,9,10,9,4,9,99,-1,8], [8]) == [0]
+    @testset "run_amplifiers!" begin
+        @test run_amplifiers!(setup_amplifiers([3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0], [4,3,2,1,0])) == 43210
+        @test run_amplifiers!(setup_amplifiers([3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0], [0,1,2,3,4])) == 54321
+        @test run_amplifiers!(setup_amplifiers([3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0], [1,0,4,3,2])) == 65210
 
-        @test run([3,3,1108,-1,8,3,4,3,99], [8]) == [1]
-        @test run([3,3,1108,-1,8,3,4,3,99], [9]) == [0]
-        @test run([3,3,1107,-1,8,3,4,3,99], [7]) == [1]
-        @test run([3,3,1107,-1,8,3,4,3,99], [8]) == [0]
-
-        @test run([3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9], [0]) == [0]
-        @test run([3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9], [5]) == [1]
-        @test run([3,3,1105,-1,9,1101,0,0,12,4,12,99,1], [0]) == [0]
-        @test run([3,3,1105,-1,9,1101,0,0,12,4,12,99,1], [5]) == [1]
+        @test run_amplifiers!(setup_amplifiers([3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5], [9,8,7,6,5])) == 139629729
+        @test run_amplifiers!(setup_amplifiers([3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10], [9,7,8,5,6])) == 18216
     end
 end
 
 solve()
-
