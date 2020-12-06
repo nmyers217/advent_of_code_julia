75d5e1685850299b5017d9373558acac1ac4a3c5
nmyers217 <nick.b.myers@protonmail.com>
2020-12-06 00:52:47 -0600

    Simplify day 6 a bit

--- 2020/src/day_06.jl
+++ 2020/src/day_06.jl
@@ -1,26 +1,10 @@
-struct Person
-    yesses::Set{Char}
-    Person(str::AbstractString) = new(Set([s[1] for s in split(str, "")]))
-end
-
 function solve()
     input = strip(read("2020/res/day_06.txt", String))
-    groups = [map(Person, split(group, "\n")) for group in split(input, "\n\n")]
-
-    part_one = begin
-        yes_counts = map(groups) do group
-            length(union([person.yesses for person in group]...))
-        end
-        sum(yes_counts)
+    groups = map(split(input, "\n\n")) do group
+        [split(person, "") for person in split(group, "\n")]
     end
-
-    part_two = begin
-        all_yes_counts = map(groups) do group
-            length(intersect([person.yesses for person in group]...))
-        end
-        sum(all_yes_counts)
-    end
-
+    part_one = sum(map(g -> length(union(g...)), groups))
+    part_two = sum(map(g -> length(intersect(g...)), groups))
     (part_one, part_two)
 end
 
