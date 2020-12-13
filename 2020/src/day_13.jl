0e8a6fc6691bfae0e8b6f078820cb057eac65965
nmyers217 <nick.b.myers@protonmail.com>
2020-12-13 02:25:39 -0600

    Finish day 13. Part 2 was really annoying.

---
+++ 2020/src/day_13.jl
@@ -0,0 +1,35 @@
+struct Schedule
+    start::Int
+    busses::Vector{Int}
+    offsets::Vector{Int}
+
+    Schedule(str::AbstractString) = begin
+        (start_str, busses_str) = split(strip(str), "\n") 
+        ids = split(busses_str, ",")
+        busses = [parse(Int, c) for c in ids if c != "x"]
+        offsets = [i - 1 for (i, c) in enumerate(ids) if c != "x"]
+        new(parse(Int, start_str), busses, offsets)
+    end
+end
+
+function solve()
+    input = read("2020/res/day_13.txt", String)
+    s = Schedule(input)
+
+    part_one = begin
+        waits = s.busses - s.start .% s.busses
+        i = argmin(waits)
+        s.busses[i] * waits[i]
+    end
+
+    part_two = begin
+        # Use chinese remainer theorem
+        p = BigInt(prod(s.busses))
+        s = sum(ai * invmod(p ÷ ni, ni) * p ÷ ni for (ni, ai) in zip(s.busses, -s.offsets))
+        mod(s, p)
+    end
+
+    part_one, part_two
+end
+
+@time solve()
