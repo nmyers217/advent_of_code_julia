84c72441b976e468b55e0ae5053c36ed3a0d57a8
nmyers217 <nick.b.myers@protonmail.com>
2020-12-22 00:15:28 -0600

    Finish day 22. Really need to stop falling asleep during these

---
+++ 2020/src/day_22.jl
@@ -0,0 +1,79 @@
+mutable struct Game
+    round::Int
+    players::Vector{Vector{Int}}
+    past_rounds::Set{Vector{Vector{Int}}}
+
+    Game(players::Vector{Vector{Int}}) = new(0, deepcopy(players), Set())
+    Game(str::AbstractString) = begin
+        players = []
+        for s in split(strip(str), "\n\n")
+            push!(players, parse.(Int, split(s, "\n")[2:end]))
+        end
+        new(0, players, Set())
+    end
+end
+
+function play!(g::Game)::Game
+    while true
+        if any(isempty, g.players)
+            return g
+        end
+        g.round += 1
+        p1 = popfirst!(g.players[1])
+        p2 = popfirst!(g.players[2])
+        winner = p1 > p2 ? g.players[1] : g.players[2]
+        append!(winner, p1 > p2 ? [p1, p2] : [p2, p1])
+    end
+    g
+end
+
+function play_recurse!(g::Game)::Int
+    while true
+        # @show g
+        if isempty(g.players[1])
+            return 2
+        elseif isempty(g.players[2])
+            return 1
+        end
+
+        if g.players in g.past_rounds
+            return 1
+        end
+        g.round += 1
+        push!(g.past_rounds, deepcopy(g.players))
+        p1 = popfirst!(g.players[1])
+        p2 = popfirst!(g.players[2])
+
+        if length(g.players[1]) >= p1 && length(g.players[2]) >= p2
+            # println("Recursing at round: $(g.round)\n")
+            winner = play_recurse!(Game([g.players[1][1:p1], g.players[2][1:p2]]))
+            # println("Player $winner won the recursion!\n")
+            append!(g.players[winner], winner == 1 ? [p1, p2] : [p2, p1])
+        else
+            winner = p1 > p2 ? g.players[1] : g.players[2]
+            append!(winner, p1 > p2 ? [p1, p2] : [p2, p1])
+        end
+    end
+end
+
+function score(g::Game, winner=-1)
+    winner = if winner == -1
+        first(p for p in g.players if !isempty(p))
+    else
+        g.players[winner]
+    end
+    sum(n * i for (n, i) in zip(winner, length(winner):-1:1))
+end
+
+function solve()
+    input = read("2020/res/day_22.txt", String)
+    part_one = input |> Game |> play! |> score
+    part_two = begin
+        g = Game(input)
+        winner = play_recurse!(g)
+        score(g, winner)
+    end
+    part_one, part_two
+end
+
+@time solve()
