ce6946af9fbdff0a86188c1a7c1ff35917a2c157
nmyers217 <nick.b.myers@protonmail.com>
2020-12-27 19:03:35 -0600

    Finish 2019 day 20

--- 2019/src/day_20.jl
+++ 2019/src/day_20.jl
@@ -1,78 +1,67 @@
 input = read(joinpath(@__DIR__, "../res", replace(basename(@__FILE__), "jl" => "txt")), String)
-
 test_input = """
-                   A
-                   A
-  #################.#############
-  #.#...#...................#.#.#
-  #.#.#.###.###.###.#########.#.#
-  #.#.#.......#...#.....#.#.#...#
-  #.#########.###.#####.#.#.###.#
-  #.............#.#.....#.......#
-  ###.###########.###.#####.#.#.#
-  #.....#        A   C    #.#.#.#
-  #######        S   P    #####.#
-  #.#...#                 #......VT
-  #.#.#.#                 #.#####
-  #...#.#               YN....#.#
-  #.###.#                 #####.#
-DI....#.#                 #.....#
-  #####.#                 #.###.#
-ZZ......#               QG....#..AS
-  ###.###                 #######
-JO..#.#.#                 #.....#
-  #.#.#.#                 ###.#.#
-  #...#..DI             BU....#..LF
-  #####.#                 #.#####
-YN......#               VT..#....QG
-  #.###.#                 #.###.#
-  #.#...#                 #.....#
-  ###.###    J L     J    #.#.###
-  #.....#    O F     P    #.#...#
-  #.###.#####.#.#####.#####.###.#
-  #...#.#.#...#.....#.....#.#...#
-  #.#####.###.###.#.#.#########.#
-  #...#.#.....#...#.#.#.#.....#.#
-  #.###.#####.###.###.#.#.#######
-  #.#.........#...#.............#
-  #########.###.###.#############
-           B   J   C
-           U   P   P
+         A
+         A
+  #######.#########
+  #######.........#
+  #######.#######.#
+  #######.#######.#
+  #######.#######.#
+  #####  B    ###.#
+BC...##  C    ###.#
+  ##.##       ###.#
+  ##...DE  F  ###.#
+  #####    G  ###.#
+  #########.#####.#
+DE..#######...###.#
+  #.#########.###.#
+FG..#########.....#
+  ###########.#####
+             Z
+             Z
 """
 
-function solve()
-    grid = begin
-        lines = split(input, "\n", keepempty=false)
-        rows, cols = length(lines), maximum(length, lines)
-        result = fill(' ', (rows, cols))
-        for (y, row) in enumerate(lines), (x, c) in enumerate(row)
-            result[y, x] = c
-        end
-        result
+const dirs = [CartesianIndex(y, x) for (x, y) in [[0, -1], [1, 0], [0, 1], [-1, 0]]]
+const Grid = Array{Char,2}
+const PortalCache = Dict{Set{Char},Dict{CartesianIndex{2},Symbol}}
+
+function Grid(str::AbstractString)::Grid
+    lines = split(str, "\n", keepempty=false)
+    rows, cols = length(lines), maximum(length, lines)
+    result = fill(' ', (rows, cols))
+    for (y, row) in enumerate(lines), (x, c) in enumerate(row)
+        result[y, x] = c
     end
+    result
+end
 
-    dirs = [CartesianIndex(y, x) for (x, y) in [[0, -1], [1, 0], [0, 1], [-1, 0]]]
-    portalcache = begin
-        result = Dict()
-        for i in CartesianIndices(grid)
-            if grid[i] != '.'
-                continue
-            end
-
-            letters = [d for d in dirs if grid[i + d] in 'A':'Z']
-
-            if isempty(letters)
-                continue
-            end
-
-            key = Set([grid[i + letters[1]], grid[i + letters[1] * 2]])
-            push!(get!(result, key, Set()), i)
+function PortalCache(grid::Grid)::PortalCache
+    result = Dict()
+    for i in CartesianIndices(grid)
+        if grid[i] != '.'
+            continue
         end
-        result
-    end
 
-    source = first(portalcache[Set(['A'])])
-    target = first(portalcache[Set(['Z'])])
+        letters = [d for d in dirs if grid[i + d] in 'A':'Z']
+
+        if isempty(letters)
+            continue
+        end
+
+        # Figure out if this point is on the outside or inside of the donut
+        y, x = Tuple(i)
+        rows, cols = size(grid, 1), size(grid, 2)
+        location = y <= 3 || y >= rows - 2 || x <= 3 || x >= cols - 2 ? :outside : :inside
+
+        key = Set([grid[i + letters[1]], grid[i + letters[1] * 2]])
+        get!(result, key, Dict())[i] = location
+    end
+    result
+end
+
+function bfs(grid::Grid, pc::PortalCache)
+    source = first(pc[Set(['A'])])[1]
+    target = first(pc[Set(['Z'])])[1]
     prev = Dict()
     visited = Set([source])
     q = [source]
@@ -91,13 +80,12 @@ function solve()
                 continue
             end
 
-            # push!(visited, neighbor)
             next = if grid[neighbor] in 'A':'Z'
                 portal = Set([grid[neighbor], grid[neighbor + d]])
                 if portal in [Set(['A']), Set(['Z'])]
                     continue
                 end
-                first(setdiff(portalcache[portal], Set([cur])))
+                first(setdiff(keys(pc[portal]), Set([cur])))
             else
                 neighbor
             end
@@ -105,6 +93,7 @@ function solve()
             if next in visited
                 continue
             end
+
             prev[next] = cur
             push!(visited, next)
             push!(q, next)
@@ -112,12 +101,74 @@ function solve()
     end
 
     path = []
-    node = first(portalcache[Set(['Z'])])
+    node = target
     while haskey(prev, node)
         push!(path, node)
         node = prev[node]
     end
     length(path)
+end
+
+function dimensional_bfs(grid::Grid, pc::PortalCache)
+    source = (first(pc[Set(['A'])])[1], 0)
+    target = (first(pc[Set(['Z'])])[1], 0)
+    prev = Dict()
+    visited = Set([source])
+    q = [source]
+
+    while !isempty(q)
+        (cur, level) = popfirst!(q)
+
+        if (cur, level) == target
+            break
+        end
+
+        for d in dirs
+            neighbor = cur + d
+
+            if grid[neighbor] == '#'
+                continue
+            end
+
+            (next, nextlevel) = if grid[neighbor] in 'A':'Z'
+                portal = Set([grid[neighbor], grid[neighbor + d]])
+                if portal in [Set(['A']), Set(['Z'])]
+                    continue
+                end
+                points = pc[portal]
+                if level == 0 && points[cur] == :outside
+                    continue
+                end
+                next = first(setdiff(keys(pc[portal]), Set([cur])))
+                nextlevel = points[next] == :outside ? level - 1 : level + 1
+                next, nextlevel
+            else
+                neighbor, level
+            end
+
+            if (next, nextlevel) in visited
+                continue
+            end
+
+            prev[(next, nextlevel)] = (cur, level)
+            push!(visited, (next, nextlevel))
+            push!(q, (next, nextlevel))
+        end
+    end
+
+    path = []
+    node = target
+    while haskey(prev, node)
+        push!(path, node)
+        node = prev[node]
+    end
+    length(path)
+end
+
+function solve()
+    grid = Grid(input)
+    portalcache = PortalCache(grid)
+    bfs(grid, portalcache), dimensional_bfs(grid, portalcache)
 end
 
 @time @show solve()
