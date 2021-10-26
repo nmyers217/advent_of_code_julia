724adae0bbd564cf772706f39c7ab5eeb46bb1c9
nmyers217 <nick.b.myers@protonmail.com>
2021-10-25 23:26:43 -0500

    2015 day 6

---
+++ 2015/src/day_06.jl
@@ -0,0 +1,28 @@
+function solve()
+    input = read(joinpath(@__DIR__, "../res", replace(basename(@__FILE__), "jl" => "txt")), String)
+
+    mapgrid(f, grid) = begin
+        result = deepcopy(grid)
+        for m in eachmatch(r"(.*) (\d+),(\d+) through (\d+),(\d+)", input)
+            (op, startx, starty, endx, endy) = m.captures
+            (sx, sy) = [parse(Int, x) + 1 for x in (startx, starty)]
+            (ex, ey) = [parse(Int, x) + 1 for x in (endx, endy)]
+            for y in sy:ey, x in sx:ex
+                result[y, x] = f(op, result[y, x])
+            end
+        end
+        result
+    end
+
+    partone = mapgrid(fill(false, (1000, 1000))) do op, val
+        if op == "turn off" 0 elseif op == "turn on" 1 else !val end
+    end |> count
+
+    parttwo = mapgrid(fill(0, (1000, 1000))) do op, val
+        if op == "turn off" max(0, val - 1) elseif op == "turn on" val + 1 else val + 2 end
+    end |> sum
+
+    partone, parttwo
+end
+
+@time solve()
