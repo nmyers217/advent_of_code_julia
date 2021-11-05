30dfce52229979b940a4b39e81aa560f78e27e22
nmyers217 <nick.b.myers@protonmail.com>
2021-11-05 16:29:30 -0500

    2015 day 14 was meh

---
+++ 2015/src/day_14.jl
@@ -0,0 +1,32 @@
+function solve()
+    input = read(joinpath(@__DIR__, "../res", replace(basename(@__FILE__), "jl" => "txt")), String)
+
+    reindeer = reduce(eachmatch(r"(\w+) .+ (\d+) .+ (\d+) .+ (\d+) .+", input), init=[]) do acc, m
+        push!(acc, tuple([parse(Int, n) for n in m.captures[2:end]]...))
+    end
+
+    dist(elapsed, reindeer) = begin
+        (speed, traveltime, resttime) = reindeer
+        cycletime = traveltime + resttime
+        cycles = convert(Int64, floor(elapsed / cycletime))
+        (cycles * traveltime + min(traveltime, rem(elapsed, cycletime))) * speed
+    end
+
+    partone = maximum(r -> dist(2503, r), reindeer)
+
+    parttwo = begin
+        scores = zeros(length(reindeer))
+        for e in 1:2503
+            distances = [dist(e, r) for r in reindeer] 
+            best = maximum(distances)
+            for (i, dist) in enumerate(distances)
+                if dist == best scores[i] += 1 end
+            end
+        end
+        convert(Int, maximum(scores))
+    end
+
+    partone, parttwo
+end
+
+@time solve()
