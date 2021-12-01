321a24d945e2b1bbb80ec81e1afb3f0c5244abd2
nmyers217 <nick.b.myers@protonmail.com>
2021-12-01 09:58:35 -0600

    start 2021 🤮

---
+++ 2021/src/day_01.jl
@@ -0,0 +1,34 @@
+function solve()
+    input = read(joinpath(@__DIR__, "../res", replace(basename(@__FILE__), "jl" => "txt")), String)
+    nums = parse.(Int, split(strip(input), "\n"))
+
+    partone = begin
+        result = 0
+        for i in 2:length(nums)
+            if nums[i] > nums[i-1]; result += 1 end
+        end
+        result
+    end
+
+    parttwo = begin
+        subtotals = []
+        for i in eachindex(nums)
+            total = 0
+            for j in i:i+2;
+                if j > length(nums) continue end
+                total += nums[j]
+            end
+            push!(subtotals, total)
+        end
+
+        result = 0
+        for i in 2:length(subtotals)
+            if subtotals[i] > subtotals[i-1]; result += 1 end
+        end
+        result
+    end
+
+    partone, parttwo
+end
+
+@time solve()
