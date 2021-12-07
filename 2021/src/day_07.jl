d8b0cb97d1f861e60a932e346ccb166921b977ce
nmyers217 <nick.b.myers@protonmail.com>
2021-12-06 23:15:44 -0600

    day 7, thank you broadcasting

---
+++ 2021/src/day_07.jl
@@ -0,0 +1,16 @@
+function solve()
+    input = read(joinpath(@__DIR__, "../res", replace(basename(@__FILE__), "jl" => "txt")), String)
+    positions = parse.(Int, split(strip(input), ","))
+
+    solve(f) = begin
+        (start, stop) = extrema(positions)
+        minimum(sum(f.(positions, target)) for target in start:stop)
+    end
+
+    distance(p, target) = abs(p - target)
+    sumcost(p, target) = sum(1:distance(p, target))
+
+    solve(distance), solve(sumcost)
+end
+
+@time solve()
