5985e41380916e33774e60b484931b00b8c61a8f
nmyers217 <nickbmyers217@gmail.com>
2021-12-12 13:59:37 -0600

    clean up day 12

--- 2021/src/day_12.jl
+++ 2021/src/day_12.jl
@@ -8,54 +8,45 @@ function getgraph(input)
     result
 end
 
-
 function solve()
     input = read(joinpath(@__DIR__, "../res", replace(basename(@__FILE__), "jl" => "txt")), String)
 
     graph = getgraph(input)
 
-    partone = begin
-        numpaths(cur, dest; seen=[]) = begin
-            if cur == dest return 1 end
+    partone(cur, dest; seen=[]) = begin
+        if cur == dest return 1 end
 
-            result = 0
-            for next in graph[cur]
-                if next == "start" continue end
-                if next in seen continue end
-                nextseen = lowercase(next) == next ? [seen; next] : seen
-                result += numpaths(next, dest, seen=nextseen)
-            end
-            result
+        result = 0
+        for next in graph[cur]
+            if next == "start" continue end
+            if next in seen continue end
+            nextseen = lowercase(next) == next ? [seen; next] : seen
+            result += partone(next, dest, seen=nextseen)
         end
-
-        numpaths("start", "end")
+        result
     end
 
-    parttwo = begin
-        numpathsnew(cur, dest; seen=Dict()) = begin
-            if cur == dest return 1 end
+    parttwo(cur, dest; seen=Dict()) = begin
+        if cur == dest return 1 end
 
-            result = 0
-            for next in graph[cur]
-                if next == "start" continue end
-                if haskey(seen, next) && 2 in values(seen)
-                    continue
-                end
-
-                nextseen = copy(seen)
-                if lowercase(next) == next
-                    get!(nextseen, next, 0)
-                    nextseen[next] += 1
-                end
-                result += numpathsnew(next, dest, seen=nextseen)
+        result = 0
+        for next in graph[cur]
+            if next == "start" continue end
+            if haskey(seen, next) && 2 in values(seen)
+                continue
             end
-            result
-        end
 
-        numpathsnew("start", "end")
+            nextseen = copy(seen)
+            if lowercase(next) == next
+                get!(nextseen, next, 0)
+                nextseen[next] += 1
+            end
+            result += parttwo(next, dest, seen=nextseen)
+        end
+        result
     end
 
-    partone, parttwo
+    partone("start", "end"), parttwo("start", "end")
 end
 
 @time solve()
