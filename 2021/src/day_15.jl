72bb91e934121314f19d543beb24d228b41cbd54
nmyers217 <nick.b.myers@protonmail.com>
2021-12-15 11:20:54 -0600

    cleanup for day 15

--- 2021/src/day_15.jl
+++ 2021/src/day_15.jl
@@ -12,32 +12,24 @@ end
 
 function dijkstra(grid)
     dirs = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    rows, cols = size(grid, 1), size(grid, 2)
+
     queue = [(0, (1, 1))]
     seen = Set([(1, 1)])
-
-    rows, cols = size(grid, 1), size(grid, 2)
     while !isempty(queue)
         (score, (x, y)) = popfirst!(queue)
 
-        if (x, y) == (cols, rows)
-            return score
-        end
+        if (x, y) == (cols, rows) return score end
 
         for d in dirs
             (nx, ny) = (x, y) .+ d
-
-            if ny < 1 || ny > rows || nx < 1 || nx > cols
-                continue
-            end
-            if (nx, ny) in seen
-                continue
-            end
-
+            if ny < 1 || ny > rows || nx < 1 || nx > cols continue end
+            if (nx, ny) in seen continue end
             push!(queue, (score + grid[ny, nx], (nx, ny)))
             push!(seen, (nx, ny))
         end
 
-        sort!(queue, by=x -> (x[1]))
+        sort!(queue, by=first)
     end
 end
 
